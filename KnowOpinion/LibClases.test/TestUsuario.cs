0980be11dc09affd4d446b03a3881bedcadf97be
jng0020 <jng0020@alu.ubu.es>
2018-10-03 18:54:59 +0200

    commit 5 - tests usuario añadidos

--- KnowOpinion/LibClases.test/TestUsuario.cs
+++ KnowOpinion/LibClases.test/TestUsuario.cs
@@ -10,6 +10,8 @@ namespace LibClases.test
         [TestMethod]
         public void TestConstructorSetyGet()
         {
+            //Prueba Get Correcto
+
             Usuario u = new Usuario(1,"mario","ubierna","cuenta","mario@alu.ubu.es");
 
             Assert.AreEqual(u.IdUsuario, 1);
@@ -21,6 +23,39 @@ namespace LibClases.test
             Assert.AreEqual(u.Cuenta, "cuenta");
 
             Assert.AreEqual(u.EMail, "mario@alu.ubu.es");
+
+            //Prueba Get incorrecto
+
+            Usuario x = new Usuario(3, "pepe", "suarez", "acc", "pepe@alu.ubu.es");
+
+            Assert.AreNotEqual(x.IdUsuario, 1);
+
+            Assert.AreNotEqual(x.Nombre, "mario");
+
+            Assert.AreNotEqual(x.Apellido, "ubierna");
+
+            Assert.AreNotEqual(x.Cuenta, "cuenta");
+
+            Assert.AreNotEqual(x.EMail, "mario@alu.ubu.es");
+
+            //Prueba Set Correcto
+
+            Assert.AreEqual(u.IdUsuario = 2, 2);
+            
+            Assert.AreEqual(u.Nombre = "jorge", "jorge");
+
+            Assert.AreEqual(u.Apellido = "navarro", "navarro");
+
+            Assert.AreEqual(u.Cuenta = "cuenta", "cuenta");
+
+            Assert.AreEqual(x.EMail = "jorge@alu.ubu.es", "jorge@alu.ubu.es");
+
+            //Prueba Asignar Pass
+
+            u.asignarPass("hola");
+
+            Assert.AreEqual(u.comprobarPass("hola"), true);
+
         }
 
     }
