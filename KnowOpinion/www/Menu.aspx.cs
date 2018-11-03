3f30716515ed68f9a81f22b42e9898e1d53965c0
Mario Ubierna <mus0007@alu.ubu.es>
2018-11-03 17:55:18 +0100

    APLICACION 3.0

--- KnowOpinion/www/Menu.aspx.cs
+++ KnowOpinion/www/Menu.aspx.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LibClases;
 
 namespace www
 {
     public partial class Menu : System.Web.UI.Page
     {
+        DataBase db;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            db = (DataBase)Session["db"];
+            string usuario = (string)Session["CuentaUsuario"];
+            if (db.getUsuario(usuario) != null)
+            {
+                db = (DataBase)Session["db"];
+                if (db == null)
+                {
+                    db = new DataBase();
+                    Session["db"] = db;
+                }
+              
+            }
+            else
+            {
+                Response.BufferOutput = true;
+                Response.Redirect("InicioSesion.aspx");
+            }
         }
 
         protected void btnadd_Click(object sender, EventArgs e)
